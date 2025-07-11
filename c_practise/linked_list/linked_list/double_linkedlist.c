88114b02b95793e948882a617d9a320d4fbbc4df
vividdesi <rahulnani988@gmail.com>
2025-07-11 15:19:28 +0530

    done basic double linkedlist

---
+++ c_practise/linked_list/linked_list/double_linkedlist.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include<stdlib.h>
+typedef struct node {
+  struct node *prev;
+  int data;
+  struct node *next;
+}node;
+
+node *create_node()
+{ 
+    int data;
+    node *temp=NULL;
+    printf("Enter the data\n");
+    scanf("%d",&data);
+    temp=malloc(sizeof(node));
+    temp->prev=NULL;
+    temp->data=data;
+    temp->next=NULL;
+    return temp;
+}
+
+node *insert_end(node *start)
+{
+    node *temp=NULL,*p=start;
+    temp=create_node();
+    if(start == NULL)
+    {
+        start=temp;
+        return start;
+    }
+    else
+    {
+        while(p->next != NULL)
+        {
+            p=p->next;
+        }
+        p->next=temp;
+        temp->prev=p;
+        return start;
+    }
+}
+
+void display(node *start)
+{
+    while(start != NULL)
+    {
+        printf("<-%d->",start->data);
+        start=start->next;
+    }
+}
+int main() {
+ node *start=NULL;
+ for(int i=0;i<4;i++)
+ {
+     start=insert_end(start);
+ }
+ display(start);
+    return 0;
+}
